5a9d55eec674b82d991fa3264033e26de7de02e4
malay <malaydev207@gmail.com>
2023-05-28 00:33:23 +0530

    changes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const connect = require("./db");
 const express = require('express')
+require('dotenv').config();
 connect();
 const app = express();
 const cors = require('cors')
 
 app.use(cors())
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/auth", require('./routes/auth'))
 app.use("/api/note", require('./routes/note'))
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
 })
