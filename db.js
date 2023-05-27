c488da612362173cfee27e4af2eb67ef1694d56e
malay <malaydev207@gmail.com>
2023-05-28 00:39:40 +0530

    changes1

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const uri = "mongodb://localhost:27017/Notebook"
+require('dotenv').config();
+const uri = process.env.URI
 const connect = () => {
     mongoose.connect(uri)
         .then(() => console.log("Database connected"))
